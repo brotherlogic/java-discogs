e78c4341ceb1355717291b0420616f836ce20b76
Simon Tucker <brotherlogic@gmail.com>
2015-06-17 19:53:41 -0700

    Added ability to query median price of a release

--- src/main/java/com/brotherlogic/discogs/Release.java
+++ src/main/java/com/brotherlogic/discogs/Release.java
@@ -1,5 +1,22 @@
 package com.brotherlogic.discogs;
 
-public class Release {
+import java.io.IOException;
 
+import com.google.gson.JsonElement;
+
+import com.brotherlogic.discogs.web.WebInterface;
+
+public class Release extends DiscogsEntity {
+
+    private long id;
+
+    public Release(long idValue) {
+	id = idValue;
+    }
+
+    public int getMedianPrice(WebInterface interf) throws IOException {
+	//We're going to return the suggested price for VG+
+	JsonElement reply = interf.get("marketplace/price_suggestions/" + id);
+	return (int)(reply.getAsJsonObject().get("Very Good Plus (VG+)").getAsJsonObject().get("value").getAsDouble() * 100);
+    }
 }
